89e6c5fb3517c83b58a12036220328440eeec111
Khalid Masum <khalid.masum.92@gmail.com>
2022-06-21 18:12:27 +0600

    add all trace configs
    Signed-off-by: Khalid Masum <khalid.masum.92@gmail.com>

--- easy_enable_configs_for_debug.sh
+++ easy_enable_configs_for_debug.sh
@@ -8,3 +8,22 @@
 ./scripts/config -d CONFIG_PANIC_ON_OOPS 
 ./scripts/config -e CONFIG_FRAME_POINTER
 
+#TRACING
+./scripts/config -e CONFIG_DMA_FENCE_TRACE
+./scripts/config -e CONFIG_STACKTRACE_BUILD_ID
+./scripts/config -e CONFIG_RCU_TRACE 
+./scripts/config -e CONFIG_FUNCTION_TRACER
+./scripts/config -e CONFIG_STACK_TRACER
+./scripts/config -e CONFIG_IRQSOFF_TRACER 
+./scripts/config -e CONFIG_PREEMPT_TRACER 
+./scripts/config -e CONFIG_SCHED_TRACER
+./scripts/config -e CONFIG_HWLAT_TRACER
+./scripts/config -e CONFIG_OSNOISE_TRACER
+./scripts/config -e CONFIG_TIMERLAT_TRACER
+./scripts/config -e CONFIG_MMIOTRACE
+./scripts/config -e CONFIG_FTRACE_SYSCALLS
+./scripts/config -e CONFIG_TRACER_SNAPSHOT
+./scripts/config -e CONFIG_TRACEPOINT_BENCHMARK 
+./scripts/config -e CONFIG_TRACE_EVAL_MAP_FILE 
+./scripts/config -e CONFIG_FTRACE_STARTUP_TEST 
+
